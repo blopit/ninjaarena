6e3d6526033650470eea32e9fadd7e5644b88c8b
Shren <patel.shrenil@gmail.com>
2016-10-29 16:38:12 -0400

    added phaser stuff

--- public/js/Player.js
+++ public/js/Player.js
@@ -1,57 +1,82 @@
 /**************************************************
 ** GAME PLAYER CLASS
 **************************************************/
-var Player = function(startX, startY) {
-	var x = startX,
-		y = startY,
-		id,
-		moveAmount = 2;
+var Player = function(room, startX, startY) {
+  var x = startX,
+    y = startY,
+    id,
+    moveAmount = 300,
+    game = room,
+    sprite;
 
-	var getX = function() {
-	    return x;
-	};
+  var create = function() {
+    sprite = game.add.sprite(x, y, 'dude');
+    game.physics.arcade.enable(sprite);
+  };
 
-	var getY = function() {
-	    return y;
-	};
+  var destroy = function() {
+    sprite.kill();
+  }
 
-	var setX = function(newX) {
-	    x = newX;
-	};
+  var getX = function() {
+    if (sprite) {
+      return sprite.position.x;
+    } else {
+      return x;
+    }
+  };
 
-	var setY = function(newY) {
-	    y = newY;
-	};
+  var getY = function() {
+    if (sprite) {
+      return sprite.position.y;
+    } else {
+      return y;
+    }
+  };
 
-	var update = function(keys) {
-		var prevX = x,
-    	prevY = y;
-		// Up key takes priority over down
-		if (keys.up) {
-			y -= moveAmount;
-		} else if (keys.down) {
-			y += moveAmount;
-		};
+  var setX = function(newX) {
+    x = newX;
+    if (sprite) {
+      sprite.position.x = x;
+    }
+  };
 
-		// Left key takes priority over right
-		if (keys.left) {
-			x -= moveAmount;
-		} else if (keys.right) {
-			x += moveAmount;
-		};
-		return (prevX != x || prevY != y) ? true : false;
-	};
+  var setY = function(newY) {
+    y = newY;
+    if (sprite) {
+      sprite.position.y = y;
+    }
+  };
 
-	var draw = function(ctx) {
-		ctx.fillRect(x-5, y-5, 10, 10);
-	};
+  var update = function(cursors) {
 
-	return {
-		getX: getX,
-		getY: getY,
-		setX: setX,
-		setY: setY,
-		update: update,
-		draw: draw
-	}
+    sprite.body.velocity.x = 0;
+    sprite.body.velocity.y = 0;
+
+    // Up key takes priority over down
+    if (cursors.up.isDown) {
+      sprite.body.velocity.y = -moveAmount;
+    } else if (cursors.down.isDown) {
+      sprite.body.velocity.y = moveAmount;
+    };
+
+    // Left key takes priority over right
+    if (cursors.left.isDown) {
+      sprite.body.velocity.x = -moveAmount;
+    } else if (cursors.right.isDown) {
+      sprite.body.velocity.x = moveAmount;
+    };
+    //return true;
+    return (sprite.body.velocity.x != 0 || sprite.body.velocity.y != 0) ? true : false;
+  };
+
+  return {
+    getX: getX,
+    getY: getY,
+    setX: setX,
+    setY: setY,
+    update: update,
+    create: create,
+    destroy: destroy
+  }
 };
--- public/js/game.js
+++ public/js/game.js
@@ -1,83 +1,95 @@
 /**************************************************
 ** GAME VARIABLES
 **************************************************/
-var canvas,			// Canvas DOM element
-	ctx,			// Canvas rendering context
-	keys,			// Keyboard input
-	localPlayer,	// Local player
-	remotePlayers,
-	socket;
-
-
+var
+  localPlayer,  // Local player
+  remotePlayers,
+  socket,
+  cursors,
+  game;
 /**************************************************
 ** GAME INITIALISATION
 **************************************************/
 function init() {
-	// Declare the canvas and rendering context
-	canvas = document.getElementById("gameCanvas");
-	ctx = canvas.getContext("2d");
+  console.log("init");
+  // Declare the canvas and rendering context
+  //canvas = document.getElementById("gameCanvas");
+  //ctx = canvas.getContext("2d");
 
-	// Maximise the canvas
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+  // Maximise the canvas
+  //canvas.width = window.innerWidth;
+  //canvas.height = window.innerHeight;
 
-	// Initialise keyboard controls
-	keys = new Keys();
+  // Initialise keyboard controls
+  //keys = new Keys();
 
-	// Calculate a random start position for the local player
-	// The minus 5 (half a player size) stops the player being
-	// placed right on the egde of the screen
-	var startX = Math.round(Math.random()*(canvas.width-5)),
-		startY = Math.round(Math.random()*(canvas.height-5));
+  // Calculate a random start position for the local player
+  // The minus 5 (half a player size) stops the player being
+  // placed right on the egde of the screen
 
-	// Initialise the local player
-	localPlayer = new Player(startX, startY);
-	remotePlayers = [];
 
-	socket = io("http://localhost:8000");
+  // Initialise the local player
+  game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+  var startX = Math.round(Math.random()*(512)),
+    startY = Math.round(Math.random()*(512));
 
-	// Start listening for events
-	setEventHandlers();
+  localPlayer = new Player(game, startX, startY);
+  remotePlayers = [];
+
+  socket = io("http://localhost:8000");
+
+  // Start listening for events
+  setEventHandlers();
 };
 
+
+function preload() {
+  game.load.spritesheet('dude', 'assets/images/dude.png', 32, 48);
+}
+
+function create() {
+  console.log('create');
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  cursors = game.input.keyboard.createCursorKeys();
+  console.log(localPlayer);
+  localPlayer.create();
+
+  //  Our two animations, walking left and right.
+  //player.animations.add('left', [0, 1, 2, 3], 10, true);
+  //player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+
+
+}
+
+function update() {
+
+  if (localPlayer.update(cursors)) {
+    socket.emit("move player", {x: localPlayer.getX(), y: localPlayer.getY()});
+  };
+
+}
 
 /**************************************************
 ** GAME EVENT HANDLERS
 **************************************************/
 var setEventHandlers = function() {
-	// Keyboard
-	window.addEventListener("keydown", onKeydown, false);
-	window.addEventListener("keyup", onKeyup, false);
 
-	// Window resize
-	window.addEventListener("resize", onResize, false);
+  // Window resize
+  window.addEventListener("resize", onResize, false);
 
-	socket.on("connect", onSocketConnected);
-	socket.on("disconnect", onSocketDisconnect);
-	socket.on("new player", onNewPlayer);
-	socket.on("move player", onMovePlayer);
-	socket.on("remove player", onRemovePlayer);
-};
-
-// Keyboard key down
-function onKeydown(e) {
-	if (localPlayer) {
-		keys.onKeyDown(e);
-	};
-};
-
-// Keyboard key up
-function onKeyup(e) {
-	if (localPlayer) {
-		keys.onKeyUp(e);
-	};
+  socket.on("connect", onSocketConnected);
+  socket.on("disconnect", onSocketDisconnect);
+  socket.on("new player", onNewPlayer);
+  socket.on("move player", onMovePlayer);
+  socket.on("remove player", onRemovePlayer);
 };
 
 // Browser window resize
 function onResize(e) {
-	// Maximise the canvas
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+  // Maximise the canvas
+  //canvas.width = window.innerWidth;
+  //canvas.height = window.innerHeight;
 };
 
 function onSocketConnected() {
@@ -92,21 +104,23 @@ function onSocketDisconnect() {
 
 function onNewPlayer(data) {
   console.log("New player connected: "+data.id);
-  var newPlayer = new Player(data.x, data.y);
-	newPlayer.id = data.id;
-	remotePlayers.push(newPlayer);
+  var newPlayer = new Player(game, data.x, data.y);
+  newPlayer.id = data.id;
+  newPlayer.create();
+  remotePlayers.push(newPlayer);
+
 };
 
 function onMovePlayer(data) {
-	var movePlayer = playerById(data.id);
+  var movePlayer = playerById(data.id);
 
-	if (!movePlayer) {
-	    console.log("Player not found: "+data.id);
-	    return;
-	};
+  if (!movePlayer) {
+      console.log("Player not found: "+data.id);
+      return;
+  };
 
-	movePlayer.setX(data.x);
-	movePlayer.setY(data.y);
+  movePlayer.setX(data.x);
+  movePlayer.setY(data.y);
 };
 
 function onRemovePlayer(data) {
@@ -119,6 +133,7 @@ function onRemovePlayer(data) {
   };
 
   // Remove player from array
+  removePlayer.destroy();
   remotePlayers.splice(remotePlayers.indexOf(removePlayer), 1);
 };
 
@@ -130,41 +145,4 @@ function playerById(id) {
   };
 
   return false;
-};
-
-/**************************************************
-** GAME ANIMATION LOOP
-**************************************************/
-function animate() {
-	update();
-	draw();
-
-	// Request a new animation frame using Paul Irish's shim
-	window.requestAnimFrame(animate);
-};
-
-
-/**************************************************
-** GAME UPDATE
-**************************************************/
-function update() {
-	if (localPlayer.update(keys)) {
-    socket.emit("move player", {x: localPlayer.getX(), y: localPlayer.getY()});
-	};
-};
-
-
-/**************************************************
-** GAME DRAW
-**************************************************/
-function draw() {
-	// Wipe the canvas clean
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-	// Draw the local player
-	localPlayer.draw(ctx);
-	var i;
-	for (i = 0; i < remotePlayers.length; i++) {
-	    remotePlayers[i].draw(ctx);
-	};
 };
